4cba6f2b6f409d7e97a33cfdf85249da607c5759
kilian henry <kilian.henry.pro@gmail.com>
2020-09-14 19:23:18 +0200

    Generated with JHipster 6.4.1

---
+++ src/main/java/fr/henry/cv/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package fr.henry.cv.config;
---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import './vendor';
+import { CvWebHenrySharedModule } from 'app/shared/shared.module';
+import { CvWebHenryCoreModule } from 'app/core/core.module';
+import { CvWebHenryAppRoutingModule } from './app-routing.module';
+import { CvWebHenryHomeModule } from './home/home.module';
+import { CvWebHenryEntityModule } from './entities/entity.module';
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+import { JhiMainComponent } from './layouts/main/main.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
+import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
+import { ErrorComponent } from './layouts/error/error.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    CvWebHenrySharedModule,
+    CvWebHenryCoreModule,
+    CvWebHenryHomeModule,
+    // jhipster-needle-angular-add-module JHipster will add new module here
+    CvWebHenryEntityModule,
+    CvWebHenryAppRoutingModule
+  ],
+  declarations: [JhiMainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
+  bootstrap: [JhiMainComponent]
+})
+export class CvWebHenryAppModule {}
